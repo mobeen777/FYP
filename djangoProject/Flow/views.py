1f126a76ca6e2166e3052c316c0621afab998af2
mobeen777 <mobeen.mobeen777@gmail.com>
2022-03-07 05:37:21 +0500

    Nothing much

--- djangoProject/Flow/views.py
+++ djangoProject/Flow/views.py
@@ -11,13 +11,12 @@ from datetime import datetime
 class GetSession(APIView):
 
     def get(self, request, *args, **kwargs):
-        session_id = getting_event_time_session()
-
-        name = flow("search", session_id[0], session_id[1])
-        calc_no_of_events(name)
-        data = {
-            "data": session_id
-        }
+        previous_events = previous_layer("search")
+        # data = {
+        #     "Target_Event": "search",
+        #     "previous_layers": previous_events,
+        # }
+        data = my_code(3, previous_events)
         return Response(data=data)
 
 
@@ -59,9 +58,9 @@ def flow_of_events(event, events, sessions):
                 if (k[1] < j[1]) and (k[1] > time_value):
                     time_value = k[1]
                     goal_event = k[0]
-
-            previous_event.append(goal_event)
-            layer.append(previous_event)
+            if goal_event != event:
+                previous_event.append(goal_event)
+                layer.append(previous_event)
 
     return layer
 
@@ -79,7 +78,7 @@ def calc_no_of_events(layer):
         count = 0
         for j in layer:
             if i == j:
-                count +=1
+                count += 1
         event_count.append((i[0], count))
 
     count_db = Funnel.objects.values('event').annotate(count=Count('event'))
@@ -87,6 +86,16 @@ def calc_no_of_events(layer):
     for i in count_db:
         for j in event_count:
             if i['event'] == j[0]:
-                per_count.append((j[0], (j[1]/i["count"])*100))
+                per_count.append((j[0], (j[1] / i["count"]) * 100))
     return per_count
+
+
+def previous_layer(target_event):
+    """Getting all layers of events"""
+
+    session_id = getting_event_time_session()
+    list_of_previous_event = flow_of_events(target_event, session_id[0], session_id[1])
+    previous_events = calc_no_of_events(list_of_previous_event)
+
+    return previous_events
 
